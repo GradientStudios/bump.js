d755d175dcbd58882513997fdbae0fbf448af428
Michael "Z" Goddard <mzgoddard@gmail.com>
2011-12-08 17:53:46 -0500

    this._super()

--- src/bump.js
+++ src/bump.js
@@ -7,20 +7,63 @@ this.Bump = {};
 
   Bump.noop = function noop() {};
 
+  function superWrap( superFunc, newFunc ) {
+    return function() {
+      var tmp = this._super,
+          ret;
+      this._super = superFunc;
+
+      ret = newFunc.apply( this, arguments );
+      this._super = tmp;
+      
+      return ret;
+    };
+  }
+
+  function walkProtoChain( prototype, func ) {
+    var ret = func( prototype );
+    return ret || walkProtoChain( Object.getPrototypeOf( prototype ), func );
+  }
+
+  var superTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+
   // The type function will be used for object inheritance.
   // Objects are instantiated with Object.create()
   Bump.type = function type( options ) {
+    options = options || {};
+    
     var exports = options.constructor || {},
+        parent = ( options.parent || {} ).prototype,
+        prototype = options.prototype || {},
         properties = options.properties || {},
         typeMethods = options.typeMethods || {},
-        key;
+        key,
+        otter;
+
+    for ( key in properties ) {
+      for ( otter in [ 'get', 'set' ] ) {
+        if ( properties[ key ][ otter ] && superTest.test( properties[ key ][ otter ] ) ) {
+          properties[ key ][ otter ] = superWrap( walkProtoChain( parent, function( prototype ) {
+            var desc = Object.getOwnPropertyDescriptor( prototype, key );
+            if ( desc ) {
+              return desc.get;
+            }
+            return undefined;
+          } ), properties[ key ][ otter ] );
+        }
+      }
+    }
 
     exports.prototype = Object.create(
-      ( options.parent || {} ).prototype,
+      parent,
       properties );
 
-    for ( key in members ) {
-      exports.prototype[key] = members[key];
+    for ( key in prototype ) {
+      if ( typeof prototype[ key ] === 'function' && superTest.test( prototype[ key ] ) ) {
+        prototype[ key ] = superWrap( parent[ key ], prototype[ key ] );
+      }
+      
+      exports.prototype[ key ] = prototype[ key ];
     }
 
     if ( !exports.prototype.init ) {
@@ -28,7 +71,7 @@ this.Bump = {};
     }
 
     for ( key in typeMethods ) {
-      exports[key] = typeMethods[key];
+      exports[ key ] = typeMethods[ key ];
     }
 
     if ( !exports.create ) {
@@ -44,4 +87,4 @@ this.Bump = {};
 
   Bump.TypedObject = Bump.type();
 
-})(this, this.Bump);
+})( this, this.Bump );
