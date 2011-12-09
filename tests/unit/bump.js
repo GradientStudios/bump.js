de78d205f6fef7851a5f0c2d3cd7de5eec6492d3
Michael "Z" Goddard <mzgoddard@gmail.com>
2011-12-08 23:33:55 -0500

    simple type instance test and instanceof supports inheritance

--- tests/unit/bump.js
+++ tests/unit/bump.js
@@ -1,10 +1,29 @@
 module( 'Bump' );
 
-test( 'Bump exists', 1 function() {
+test( 'Bump exists', 1, function() {
   ok( Bump, 'Bump exists' );
 } );
 
 test( 'functions exists', 2, function() {
   ok( Bump.noop, 'noop exists' );
   ok( Bump.type, 'type exists' );
+} );
+
+test( 'instantiate test 1', 3, function() {
+  var ObjectA = Bump.type(),
+      a = ObjectA.create();
+
+  ok( ObjectA, 'type created' );
+  ok( a, 'instance created' );
+  equal( Object.getPrototypeOf(a), ObjectA.prototype );
+} );
+
+test( 'inheritance supports instanceof', 2, function() {
+  var A = Bump.type(),
+      B = Bump.type( { parent: A } ),
+      a = A.create(),
+      b = B.create();
+
+  ok( a instanceof A.constructor, 'direct instance passes instanceof' );
+  ok( b instanceof A.constructor, 'instance of subtype passes instanceof' );
 } );
