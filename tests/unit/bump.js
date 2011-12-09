834b1070ef00f22b898c3657b3915c833da313b1
Michael "Z" Goddard <mzgoddard@gmail.com>
2011-12-08 21:22:48 -0500

    functions exists for Bump.noop and Bump.type

--- tests/unit/bump.js
+++ tests/unit/bump.js
@@ -1,6 +1,10 @@
 module( 'Bump' );
 
-// put some real tests for basic bump necessities here (like type?)
-test( 'always fail', 1 function() {
-  ok( false, 'always fail' );
+test( 'Bump exists', 1 function() {
+  ok( Bump, 'Bump exists' );
+} );
+
+test( 'functions exists', 2, function() {
+  ok( Bump.noop, 'noop exists' );
+  ok( Bump.type, 'type exists' );
 } );
