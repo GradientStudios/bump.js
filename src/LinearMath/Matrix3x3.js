e497774ea3312f492ea2c6c53a99df4b21dfaa30
Eric Li <li.zhe.hua@gmail.com>
2011-12-16 14:37:16 -0500

    Add basic Matrix3x3 functionality (#4)
    This includes
    - Instantiation
      - create
      - getIdentity
      - clone
    - Basic access functions
      - getRow
      - getColumn
      - [] accessors
    - Basic setters
      - setValue
      - setIdentity

---
+++ src/LinearMath/Matrix3x3.js
@@ -0,0 +1,155 @@
+// **Matrix3x3** is the port of the `btMatrix3x3` class in Bullet.
+
+(function( window, Bump ) {
+  Bump.Matrix3x3 = Bump.type({
+    // Given *exactly* nine arguments in row major order,
+    // initializes a 3x3 matrix.
+    init: function Matrix3x3( xx, xy, xz, yx, yy, yz, zx, zy, zz ) {
+      this.m_el0 = Bump.Vector3.create( xx, xy, xz );
+      this.m_el1 = Bump.Vector3.create( yx, yy, yz );
+      this.m_el2 = Bump.Vector3.create( zx, zy, zz );
+
+      this.m_el = [
+        Bump.Vector3.create( xx, xy, xz ),
+        Bump.Vector3.create( yx, yy, yz ),
+        Bump.Vector3.create( zx, zy, zz )
+      ];
+
+      this.setValue( xx, xy, xz,
+                     yx, yy, yz,
+                     zx, zy, zz );
+    },
+
+    // The properties for 0, 1, and 2 are used to emulate `btMatrix3x3`'s
+    // `operator[]` overload. This is purely for maintaining the interface, as
+    // they are much slower. It is faster to access the properties directly on
+    // the object.
+    properties: {
+      0: {
+        get: function() { return this.m_el0; },
+        set: function( vec3 ) { this.m_el0 = vec3; }
+      },
+
+      1: {
+        get: function() { return this.m_el1; },
+        set: function( vec3 ) { this.m_el1 = vec3; }
+      },
+
+      2: {
+        get: function() { return this.m_el2; },
+        set: function( vec3 ) { this.m_el2 = vec3; }
+      }
+    },
+
+    members: {
+      // Clones the value of the given matrix `mat` to the matrix.
+      clone: function( mat ) {
+        this.m_el0.clone( mat.m_el0 );
+        this.m_el1.clone( mat.m_el1 );
+        this.m_el2.clone( mat.m_el2 );
+
+        this.m_el[0].clone( mat.m_el[0] );
+        this.m_el[1].clone( mat.m_el[1] );
+        this.m_el[2].clone( mat.m_el[2] );
+
+        this.m[0] = mat.m[0]; this.m[1] = mat.m[1]; this.m[2] = mat.m[2];
+        this.m[3] = mat.m[3]; this.m[4] = mat.m[4]; this.m[5] = mat.m[5];
+        this.m[6] = mat.m[6]; this.m[7] = mat.m[7]; this.m[8] = mat.m[8];
+
+        this.m11 = mat.m11; this.m12 = mat.m12; this.m13 = mat.m13;
+        this.m21 = mat.m21; this.m22 = mat.m22; this.m23 = mat.m23;
+        this.m31 = mat.m31; this.m32 = mat.m32; this.m33 = mat.m33;
+
+        return this;
+      },
+
+      // Puts the *i*th column into the given `Bump.Vector3` `dest`.
+      getColumn: function( i, dest ) {
+        dest.x = this.m_el[0][i];
+        dest.y = this.m_el[1][i];
+        dest.z = this.m_el[2][i];
+        return dest;
+      },
+
+      // Puts the *i*th row into the given `Bump.Vector3` `dest`.
+      getRow: function( i, dest ) {
+        dest.clone( this[ i ] );
+        return dest;
+      },
+
+      // Given *exactly* nine arguments in row major order, sets the values of
+      // the matrix.
+      setValue: function( xx, xy, xz, yx, yy, yz, zx, zy, zz ) {
+        this.m_el0.setValue( xx, xy, xz );
+        this.m_el1.setValue( yx, yy, yz );
+        this.m_el2.setValue( zx, zy, zz );
+
+        this.m_el[0].setValue( xx, xy, xz );
+        this.m_el[1].setValue( yx, yy, yz );
+        this.m_el[2].setValue( zx, zy, zz );
+
+        this.m11 = xx; this.m12 = xy; this.m13 = xz;
+        this.m21 = yx; this.m22 = yy; this.m23 = yz;
+        this.m31 = zx; this.m32 = zy; this.m33 = zz;
+
+        this.m = [ xx, xy, xz, yx, yy, yz, zx, zy, zz ];
+
+        return this;
+      },
+
+      // Set the matrix to the identity matrix.
+      setIdentity: function() {
+        return this.setValue( 1, 0, 0,
+                              0, 1, 0,
+                              0, 0, 1 );
+      },
+
+      // Internal method for alternate initialization using three
+      // `Bump.Vector3`s
+      _initWithVectors: function( vecA, vecB, vecC ) {
+        this.m_el0 = Bump.Vector3.clone( vecA );
+        this.m_el1 = Bump.Vector3.clone( vecB );
+        this.m_el2 = Bump.Vector3.clone( vecC );
+
+        this.m_el = [
+          Bump.Vector3.clone( vecA ),
+          Bump.Vector3.clone( vecB ),
+          Bump.Vector3.clone( vecC )
+        ];
+
+        this.setValue( vecA.x, vecA.y, vecA.z,
+                       vecB.x, vecB.y, vecB.z,
+                       vecC.x, vecC.y, vecC.z );
+      }
+    },
+
+    typeMembers: {
+      // Given *up to* nine arguments in row major order, **creates** a new 3x3
+      // matrix.
+      create: function( xx, xy, xz, yx, yy, yz, zx, zy, zz ) {
+        var mat = Object.create( Bump.Matrix3x3.prototype );
+        mat.init( xx || 0, xy || 0, xz || 0,
+                  yx || 0, yy || 0, yz || 0,
+                  zx || 0, zy || 0, zz || 0 );
+        return mat;
+      },
+
+      // **Creates** a 3x3 identity matrix. **Note:** this does not return a
+      // `const` static reference like in C++.
+      getIdentity: function() {
+        var mat = Object.create( Bump.Matrix3x3.prototype );
+        mat.init( 1, 0, 0,
+                  0, 1, 0,
+                  0, 0, 1 );
+        return mat;
+      },
+
+      // **Creates** a new matrix and copies a matrix to it.
+      clone: function( mat ) {
+        var newMat = Object.create( Bump.Matrix3x3.prototype );
+        newMat._initWithVectors( mat.m_el0, mat.m_el1, mat.m_el2 );
+        return newMat;
+      }
+    }
+  });
+})( this, this.Bump );
