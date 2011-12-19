719b6c22c89fab620a93b1a00f34c0b377fece97
Andrew Dolce <andrew.dolce@gmail.com>
2011-12-19 13:48:36 -0500

    Refactored to fit new standard of passing in destination vectors #3

--- src/LinearMath/vector3.js
+++ src/LinearMath/vector3.js
@@ -52,13 +52,21 @@
         return this;
       },
 
-      add: function( vec, vec2 ) {
-        this.x = vec.x + vec2.x;
-        this.y = vec.y + vec2.y;
-        this.z = vec.z + vec2.z;
-        return this;
+      // add this to v, return result in dest if provided
+      // if not, a new vector3 is created and returned
+      add: function( vec, dest ) {
+        if( dest ) {
+          dest.x = this.x + vec.x;
+          dest.y = this.y + vec.y;
+          dest.z = this.z + vec.z;
+          return dest;
+        }
+        return Bump.Vector3.create( this.x + vec.x,
+                                    this.y + vec.y,
+                                    this.z + vec.z );
       },
 
+      // this = this + v
       addSelf: function( v ) {
         this.x += v.x;
         this.y += v.y;
@@ -66,13 +74,20 @@
         return this;
       },
 
-      subtract: function( vec, vec2 ) {
-        this.x = vec.x - vec2.x;
-        this.y = vec.y - vec2.y;
-        this.z = vec.z - vec2.z;
-        return this;
+      subtract: function( vec, dest ) {
+        if( dest ) {
+          dest.x = this.x - vec.x;
+          dest.y = this.y - vec.y;
+          dest.z = this.z - vec.z;
+          return dest;
+        }
+        return Bump.Vector3.create( this.x - vec.x,
+                                    this.y - vec.y,
+                                    this.z - vec.z );
       },
 
+      // subtract v from this, return result in dest if provided
+      // if not, a new vector3 is created and returned
       subtractSelf: function( v ) {
         this.x -= v.x;
         this.y -= v.y;
@@ -80,12 +95,18 @@
         return this;
       },
 
-      // scalar multiplication
-      multiply: function( vec, scalar ) {
-        this.x = vec.x * scalar;
-        this.y = vec.y * scalar;
-        this.z = vec.z * scalar;
-        return this;
+      // multiply this by scalar
+      // store result in dest if provided, create new vector3 if not
+      multiply: function( scalar, dest ) {
+        if( dest ) {
+          dest.x = this.x * scalar;
+          dest.y = this.y * scalar;
+          dest.z = this.z * scalar;
+          return dest;
+        }
+        return Bump.Vector3.create( this.x * scalar,
+                                    this.y * scalar,
+                                    this.z * scalar );
       },
 
       multiplySelf: function( scalar ) {
@@ -95,14 +116,21 @@
         return this;
       },
 
-      // elementwise vector multiplication
-      scale: function( vec, vec2 ) {
-        this.x = vec.x * vec2.x;
-        this.y = vec.y * vec2.y;
-        this.z = vec.z * vec2.z;
-        return this;
+      // element-wise multiplication: multiply this by the elements of vec
+      // store result in dest if provided, create new vector3 if not
+      scale: function( vec, dest ) {
+        if( dest ) {
+          dest.x = this.x * vec.x;
+          dest.y = this.y * vec.y;
+          dest.z = this.z * vec.z;
+          return dest;
+        }
+        return Bump.Vector3.create( this.x * vec.x,
+                                    this.y * vec.y,
+                                    this.z * vec.z );
       },
 
+      // element-wise multiplication in place: this = this * vec
       scaleSelf: function( vec ) {
         this.x *= vec.x;
         this.y *= vec.y;
@@ -110,14 +138,21 @@
         return this;
       },
 
-      // scalar division
-      divide: function( vec, scalar ) {
-        this.x = vec.x / scalar;
-        this.y = vec.y / scalar;
-        this.z = vec.z / scalar;
-        return this;
+      // scalar division : divide this by scalar
+      // store result in dest if provided, else create new vector3
+      divide: function( scalar, dest ) {
+        if( dest ) {
+          dest.x = this.x / scalar;
+          dest.y = this.y / scalar;
+          dest.z = this.z / scalar;
+          return dest;
+        }
+        return Bump.Vector3.create( this.x / scalar,
+                                    this.y / scalar,
+                                    this.z / scalar );
       },
 
+      // in-place scalar division
       divideSelf: function( scalar ) {
         this.x /= scalar;
         this.y /= scalar;
@@ -125,15 +160,23 @@
         return this;
       },
 
-      // elementwise vector division
+      // element-wise vector division : divide this by elements of vec
+      // store result in dest if provided, else create new vector3
       // not sure on this naming convention
-      inverseScale: function( vec, vec2 ) {
-        this.x = vec.x / vec2.x;
-        this.y = vec.y / vec2.y;
-        this.z = vec.z / vec2.z;
-        return this;
+      inverseScale: function( vec, dest ) {
+        if( dest ) {
+          dest.x = this.x / vec.x;
+          dest.y = this.y / vec.y;
+          dest.z = this.z / vec.z;
+          return dest;
+        }
+        return Bump.Vector3.create( this.x / vec.x,
+                                    this.y / vec.y,
+                                    this.z / vec.z );
       },
 
+      // element-wise vector division in place:
+      // this = this / vec
       inverseScaleSelf: function( vec ) {
         this.x /= vec.x;
         this.y /= vec.y;
@@ -146,27 +189,33 @@
         return this.x * vec.x + this.y * vec.y + this.z * vec.z;
       },
 
+      // squared magnitude of this vector
       length2: function() {
         return this.x * this.x + this.y * this.y + this.z * this.z;
       },
 
+      // magnitude of this vector
       length: function() {
         return Math.sqrt( this.x * this.x +
                           this.y * this.y +
                           this.z * this.z);
       },
 
+      // squared distance between this vector and vec
       distance2: function( vec ) {
-        return Bump.Vector3.create().subtract( vec, this ).length2();
+        return vec.subtract( this ).length2();
       },
 
+      // distance between this vector and vec
       distance: function( vec ) {
-        return Bump.Vector3.create().subtract( vec, this ).length();
+        return vec.subtract( this ).length();
       },
 
-      // altered slightly to avoid index [] notation
+      // Normalize this vector in place, safely checking for cases
+      // of division by zero.
+      // Altered slightly to avoid index [] notation.
       safeNormalize: function( ) {
-        var absMax = Bump.Vector3.create().absolute( this ).max();
+        var absMax = this.absolute().max();
 
         if( absMax > 0 ) {
           this.divideSelf( absMax );
@@ -176,60 +225,76 @@
         return this;
       },
 
+      // Normalize this vector in place.
       normalize: function( ) {
         return this.divideSelf( this.length() );
       },
 
-      normalized: function( vec ) {
-        return this.divide( vec, vec.length() );
+      // Compute normalized version of this vector. Store result in dest
+      // if provided, create new vector3 if not.
+      normalized: function( dest ) {
+        // divide function will check if dest is null
+        return this.divide( this.length(), dest );
       },
 
-      // rotate vec around wAxis by angle
-      rotate: function( vec, wAxis, angle ) {
+      // Return a rotated version of this vector, rotating around wAxis by angle
+      // Store result in dest if provided, else create new Vector3.
+      rotate: function( wAxis, angle, dest ) {
         // wAxis must be a unit length vector
-        var o = Bump.Vector3.create().multiply( wAxis, wAxis.dot( vec ) ),
-        x = Bump.Vector3.create().subtract( vec, o ),
-        y = Bump.Vector3.create().cross( wAxis, vec );
+        var o = wAxis.multiply( wAxis.dot( this ), dest ), // new temp if dest unspecified
+        x = this.subtract( o ).multiplySelf( Math.cos( angle ) ), // new temp
+        y = wAxis.cross( this ).multiplySelf( Math.sin( angle ) ); // new temp
 
-        return this.add( o, this.add( x.multiplySelf( Math.cos( angle ) ),
-                                      y.multiplySelf( Math.sin( angle ) )
-                                    )
-                       );
+        return o.addSelf( x.add( y ) );
       },
 
-      // return angle between this vector and vec
+      // Return angle between this vector and vec.
       angle: function( v ) {
         var s = Math.sqrt( this.length2() * v.length2() );
         // btFullAssert( s != btScalar( 0.0 ) )
         return Math.acos( this.dot( v ) / s );
       },
 
-      absolute: function( vec ) {
-        this.x = Math.abs( vec.x );
-        this.y = Math.abs( vec.y );
-        this.z = Math.abs( vec.z );
+      // Return a vector3 with the absolute values of this vector's elements.
+      // Store the result in dest if provided, else create a new Vector3.
+      absolute: function( dest ) {
+        if ( dest ) {
+          dest.x = Math.abs( this.x );
+          dest.y = Math.abs( this.y );
+          dest.z = Math.abs( this.z );
 
-        return this;
+          return dest;
+        }
+        return Bump.Vector3.create( Math.abs( this.x ),
+                                    Math.abs( this.y ),
+                                    Math.abs( this.z ) );
       },
 
-      // compute cross product of vec and vec2, store result in this
-      cross: function( vec, vec2 ) {
-        if( this === vec || this === vec2 ){
-          // just in case
-          var x = vec.y * vec2.z - vec.z * vec2.y,
-          y = vec.z * vec2.x - vec.x * vec2.z,
-          z = vec.x * vec2.y - vec.y * vec2.x;
-
-          this.x = x;
-          this.y = y;
-          this.z = z;
-
-          return this;
+      // Compute cross product of this and vec.
+      // Store result in dest if provided, else create new Vector3.
+      // Note that this will fail if dest === this
+      cross: function( vec, dest ) {
+        if( dest ) {
+          dest.x = this.y * vec.z - this.z * vec.y;
+          dest.y = this.z * vec.x - this.x * vec.z;
+          dest.z = this.x * vec.y - this.y * vec.x;
+          return dest;
         }
 
-        this.x = vec.y * vec2.z - vec.z * vec2.y;
-        this.y = vec.z * vec2.x - vec.x * vec2.z;
-        this.z = vec.x * vec2.y - vec.y * vec2.x;
+        return Bump.Vector3.create( this.y * vec.z - this.z * vec.y,
+                                    this.z * vec.x - this.x * vec.z,
+                                    this.x * vec.y - this.y * vec.x );
+      },
+
+      // Compute cross product of this and vec, storing result in this
+      crossSelf: function( vec ) {
+        var x = this.y * vec.z - this.z * vec.y,
+        y = this.z * vec.x - this.x * vec.z,
+        z = this.x * vec.y - this.y * vec.x;
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
 
         return this;
       },
@@ -289,19 +354,19 @@
        *  backwards...
        */
       furthestAxis: function() {
-        return Bump.Vector3.create().absolute( this ).minAxis();
+        return this.absolute().minAxis();
       },
 
       furthest: function() {
-        return Bump.Vector3.create().absolute( this ).min();
+        return this.absolute().min();
       },
 
       closestAxis: function() {
-        return Bump.Vector3.create().absolute( this ).maxAxis();
+        return this.absolute().maxAxis();
       },
 
       closest: function() {
-        return Bump.Vector3.create().absolute( this ).max();
+        return this.absolute().max();
       },
 
       // Linearly interpolate between the two vectors and place the
@@ -315,13 +380,19 @@
         return this;
       },
 
-      // Linearly interpolate between this and vec, place the result in this
-      lerp : function( vec, t ) {
-        this.x += ( vec.x - this.x ) * t;
-        this.y += ( vec.y - this.y ) * t;
-        this.z += ( vec.z - this.z ) * t;
+      // Linearly interpolate between this and vec
+      // Store the result in dest if provided, else create a new Vector3.
+      lerp : function( vec, t, dest ) {
+        if( dest ) {
+          dest.x = this.x + ( vec.x - this.x ) * t;
+          dest.y = this.y + ( vec.y - this.y ) * t;
+          dest.z = this.z + ( vec.z - this.z ) * t;
+          return dest;
+        }
 
-        return this;
+        return Bump.Vector3.create( this.x + ( vec.x - this.x ) * t,
+                                    this.y + ( vec.y - this.y ) * t,
+                                    this.z + ( vec.z - this.z ) * t );
       },
 
       // element-wise comparison of vectors ( note : ignores w values )
