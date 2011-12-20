45671a73c04bf6893658fefe6f217c3c0b0a4b70
Andrew Dolce <andrew.dolce@gmail.com>
2011-12-20 11:53:12 -0500

    Added new file for port of btBroadphaseProxy #6

---
+++ src/BulletCollision/BroadphaseCollision/BroadphaseProxy.js
@@ -0,0 +1,7 @@
+// **Bump.BroadphaseProxy** is the port of the `btBroadphaseProxy` class in
+// [Bullet](http://bulletphysics.org).
+
+(function( window, Bump ) {
+  Bump.BroadphaseProxy = Bump.type( {
+  } );
+} )( this, this.Bump );
