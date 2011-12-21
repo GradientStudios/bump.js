62aeec34fc0d105a28c8aa44cf642fb4fa2726e8
Eric Li <li.zhe.hua@gmail.com>
2011-12-20 20:57:17 -0500

    Add all the remaining quaternion methods

--- src/LinearMath/Quaternion.js
+++ src/LinearMath/Quaternion.js
@@ -7,7 +7,7 @@
 // single class in this port.
 
 (function( window, Bump ) {
-  var EPSILON = Math.pow( 2, -52 );
+  var tmpQ1, tmpQ2, tmpV1, tmpV2, tmpV3, EPSILON = Math.pow( 2, -52 );
 
   Bump.Quaternion = Bump.type({
     // Given *exactly* four arguments, initializes a quaternion.
@@ -163,15 +163,302 @@
         if ( other.m_floats[2] > this.m_floats[2] ) { this.m_floats[2] = other.m_floats[2]; }
         if ( other.m_floats[3] > this.m_floats[3] ) { this.m_floats[3] = other.m_floats[3]; }
         return this;
+      },
+
+      // ### btQuaternion members
+
+      // Sets `this` rotation given an `axis` and `angle` in radians.
+      setRotation: function( axis, angle ) {
+        var d = axis.length();
+        // btAssert( d != 0 );
+        var s = Math.sin( angle * 0.5 ) / d;
+        return this.setValue( axis.x * s, axis.y * s, axis.z * s, Math.cos( angle * 0.5 ) );
+      },
+
+      // Sets `this` rotation given `yaw`, `pitch`, and `roll` Euler angles.
+      setEuler: function( yaw, pitch, roll ) {
+        var halfYaw = yaw * 0.5,
+            halfPitch = pitch * 0.5,
+            halfRoll = roll * 0.5,
+            cosYaw = Math.cos( halfYaw ),
+            sinYaw = Math.sin( halfYaw ),
+            cosPitch = Math.cos( halfPitch ),
+            sinPitch = Math.sin( halfPitch ),
+            cosRoll = Math.cos( halfRoll ),
+            sinRoll = Math.sin( halfRoll );
+
+        this.setValue(
+          cosRoll * sinPitch * cosYaw + sinRoll * cosPitch * sinYaw,
+          cosRoll * cosPitch * sinYaw - sinRoll * sinPitch * cosYaw,
+          sinRoll * cosPitch * cosYaw - cosRoll * sinPitch * sinYaw,
+          cosRoll * cosPitch * cosYaw + sinRoll * sinPitch * sinYaw
+        );
+      },
+
+      setEulerZYX: function( yaw, pitch, roll ) {
+        var halfYaw = yaw * 0.5,
+            halfPitch = pitch * 0.5,
+            halfRoll = roll * 0.5,
+            cosYaw = Math.cos( halfYaw ),
+            sinYaw = Math.sin( halfYaw ),
+            cosPitch = Math.cos( halfPitch ),
+            sinPitch = Math.sin( halfPitch ),
+            cosRoll = Math.cos( halfRoll ),
+            sinRoll = Math.sin( halfRoll );
+
+        this.setValue(
+          sinRoll * cosPitch * cosYaw - cosRoll * sinPitch * sinYaw, // x
+          cosRoll * sinPitch * cosYaw + sinRoll * cosPitch * sinYaw, // y
+          cosRoll * cosPitch * sinYaw - sinRoll * sinPitch * cosYaw, // z
+          cosRoll * cosPitch * cosYaw + sinRoll * sinPitch * sinYaw  // formerly yzx
+        );
+      },
+
+      // ### Math operator functions
+
+      // Negates each element and stores the resulting quaternion in `dest`.
+      negate: function( dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue( -this.x, -this.y, -this.z, -this.w );
+      },
+
+      add: function( quat, dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue(
+          this.x + quat.x,
+          this.y + quat.y,
+          this.z + quat.z,
+          this.w + quat.w
+        );
+      },
+
+      addSelf: function( quat ) {
+        return this.setValue(
+          this.x + quat.x,
+          this.y + quat.y,
+          this.z + quat.z,
+          this.w + quat.w
+        );
+      },
+
+      subtract: function( quat, dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue(
+          this.x - quat.x,
+          this.y - quat.y,
+          this.z - quat.z,
+          this.w - quat.w
+        );
+      },
+
+      subtractSelf: function( quat ) {
+        return this.setValue(
+          this.x - quat.x,
+          this.y - quat.y,
+          this.z - quat.z,
+          this.w - quat.w
+        );
+      },
+
+      multiplyQuaternion: function( quat, dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue(
+          this.w * quat.x + this.x * quat.w + this.y * quat.z - this.z * quat.y,
+          this.w * quat.y + this.y * quat.w + this.z * quat.x - this.x * quat.z,
+          this.w * quat.z + this.z * quat.w + this.x * quat.y - this.y * quat.x,
+          this.w * quat.w - this.x * quat.x - this.y * quat.y - this.z * quat.z
+        );
+      },
+
+      multiplyQuaternionSelf: function( quat ) {
+        return this.setValue(
+          this.w * quat.x + this.x * quat.w + this.y * quat.z - this.z * quat.y,
+          this.w * quat.y + this.y * quat.w + this.z * quat.x - this.x * quat.z,
+          this.w * quat.z + this.z * quat.w + this.x * quat.y - this.y * quat.x,
+          this.w * quat.w - this.x * quat.x - this.y * quat.y - this.z * quat.z
+        );
+      },
+
+      multiplyVector: function( vec, dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue(
+          this.w * vec.x + this.y * vec.z - this.z * vec.y,
+          this.w * vec.y + this.z * vec.x - this.x * vec.z,
+          this.w * vec.z + this.x * vec.y - this.y * vec.x,
+          -this.x * vec.x - this.y * vec.y - this.z * vec.z
+        );
+      },
+
+      vectorMultiply: function( vec, dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue(
+          vec.x * this.w + vec.y * this.z - vec.z * this.y,
+          vec.y * this.w + vec.z * this.x - vec.x * this.z,
+          vec.z * this.w + vec.x * this.y - vec.y * this.x,
+          -vec.x * this.x - vec.y * this.y - vec.z * this.z
+        );
+      },
+
+      multiplyScalar: function( s, dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue( this.x * s, this.y * s, this.z * s, this.w * s );
+      },
+
+      multiplyScalarSelf: function( s ) {
+        return this.setValue( this.x * s, this.y * s, this.z * s, this.w * s );
+      },
+
+      divideScalar: function( s, dest ) {
+        // btAssert( s !== 0 );
+        dest = dest || Bump.Quaternion.create();
+        var i = 1 / s;
+        return dest.setValue( this.x * i, this.y * i, this.z * i, this.w * i );
+      },
+
+      divideScalarSelf: function( s ) {
+        // btAssert( s !== 0 );
+        var i = 1 / s;
+        return this.setValue( this.x * i, this.y * i, this.z * i, this.w * i );
+      },
+
+      // ### Other math functions
+
+      dot: function( quat ) {
+        return this.x * quat.x + this.y * quat.y + this.z * quat.z + this.w * quat.w;
+      },
+
+      length2: function() {
+        return this.dot( this );
+      },
+
+      length: function() {
+        return Math.sqrt( this.length2() );
+      },
+
+      normalized: function( dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return this.divideScalar( this.length(), dest );
+      },
+
+      normalize: function() {
+        return this.divideScalarSelf( this.length() );
+      },
+
+      // Get the angle between `this` quaternion and the quaternion `quat`.
+      angle: function( quat ) {
+        var s = Math.sqrt( this.length2() * quat.length2() );
+        // btAssert( s !== 0 );
+        return Math.acos( this.dot( quat ) / s );
+      },
+
+      // Get the angle of rotation represented by `this` quaternion.
+      getAngle: function() {
+        return 2 * Math.acos( this.w );
+      },
+
+      // Get the axis of the rotation represented by `this` quaternion.
+      getAxis: function( dest ) {
+        dest = dest || Bump.Vector3.create();
+        var s_squared = 1 - this.w * this.w;
+
+        // Check for divide by zero
+        if ( s_squared < 10 * EPSILON ) {
+          // Arbitrary
+          return dest.setValue( 1, 0, 0 );
+        }
+
+        var s = 1 / Math.sqrt( s_squared );
+        return dest.setValue( this.x * s, this.y * s, this.z * s );
+      },
+
+      inverse: function( dest ) {
+        dest = dest || Bump.Quaternion.create();
+        return dest.setValue( -this.x, -this.y, -this.z, this.w );
+      },
+
+      // ## Utility functions
+
+      farthest: function( quat, dest ) {
+        dest = dest || Bump.Quaternion.create();
+
+        var diff = tmpQ1, sum = tmpQ2;
+        this.subtract( quat, diff );
+        this.add( quat, sum );
+        if ( diff.dot( diff ) > sum.dot( sum ) ) {
+          return quat.clone( dest );
+        }
+        return quat.negate( dest );
+      },
+
+      nearest: function( quat, dest ) {
+        dest = dest || Bump.Quaternion.create();
+
+        var diff = tmpQ1, sum = tmpQ2;
+        this.subtract( quat, diff );
+        this.add( quat, sum );
+        if( diff.dot( diff ) < sum.dot( sum ) ) {
+          return quat.clone( dest );
+        }
+        return quat.negate( dest );
+      },
+
+      slerp: function( quat, t, dest ) {
+        dest = dest || Bump.Quaternion.create();
+
+        var theta = this.angle( quat );
+        if ( theta !== 0 ) {
+          var d = 1 / Math.sin( theta ),
+              s0 = Math.sin( ( 1 - t ) * theta ),
+              s1 = Math.sin( t * theta );
+
+          // Take care of long angle case.
+          // See [Slerp](http://en.wikipedia.org/wiki/Slerp).
+          if ( this.dot( quat ) < 0 ) {
+            return dest.setValue(
+              ( this.x * s0 + -quat.x * s1 ) * d,
+              ( this.y * s0 + -quat.y * s1 ) * d,
+              ( this.z * s0 + -quat.z * s1 ) * d,
+              ( this.w * s0 + -quat.w * s1 ) * d
+            );
+          }
+
+          return dest.setValue(
+            ( this.x * s0 + quat.x * s1 ) * d,
+            ( this.y * s0 + quat.y * s1 ) * d,
+            ( this.z * s0 + quat.z * s1 ) * d,
+            ( this.w * s0 + quat.w * s1 ) * d
+          );
+        }
+
+        return this.clone( dest );
       }
     },
 
-    // ## Static functions
+    // ## Static methods
     typeMembers: {
+
+      // ### Constructors
+
       // Given *up to* four arguments, **creates** a new quaternion.
       create: function( x, y, z, w ) {
         var quat = Object.create( Bump.Quaternion.prototype );
         quat.init( x || 0, y || 0, z || 0, w || 0 );
+        return quat;
+      },
+
+      createWithAxisAngle: function( axis, angle ) {
+        var d = axis.length();
+        // btAssert( d !== 0 );
+        var s = Math.sin( angle * 0.5 ) / d,
+            quat = Object.create( Bump.Quaternion.prototype );
+
+        quat.init( axis.x * s, axis.y * s, axis.z * s, Math.cos( angle * 0.5 ) );
+        return quat;
+      },
+
+      getIdentity: function() {
+        var quat = Object.create( Bump.Quaternion.prototype );
+        quat.init( 0, 0, 0, 1 );
         return quat;
       },
 
@@ -180,7 +467,67 @@
         var newQuat = Object.create( Bump.Quaternion.prototype );
         newQuat.init( quat.x, quat.y, quat.z, quat.w );
         return newQuat;
+      },
+
+      dot: function( q1, q2 ) {
+        return q1.dot( q2 );
+      },
+
+      length: function( quat ) {
+        return quat.length();
+      },
+
+      angle: function( q1, q2 ) {
+        return q1.angle( q2 );
+      },
+
+      inverse: function( quat ) {
+        return quat.inverse();
+      },
+
+      slerp: function( q1, q2, t ) {
+        return q1.slerp( q2, t );
+      },
+
+      quatRotation: function( rotation, v, dest ) {
+        dest = dest || Bump.Vector3.create();
+
+        var q = rotation.multiplyVector( v, tmpQ1 );
+        q.multiplyQuaternionSelf( rotation.inverse( tmpQ2 ) );
+        return dest.setValue( q.x, q.y, q.z );
+      },
+
+      shortestArcQuat: function( v0, v1, dest ) {
+        dest = dest || Bump.Quaternion.create();
+
+        var c = v0.cross( v1, tmpV1 ),
+            d = v0.dot( v1 );
+
+        if ( d < -1 + EPSILON ) {
+          var n = tmpV2, unused = tmpV3;
+          Bump.PlaneSpace1( v0, n, unused );
+          // Just pick any vector that is orthogonal to `v0`.
+          return dest.setValue( n.x, n.y, n.z, 0 );
+        }
+
+        var s = Math.sqrt( ( 1 + d ) * 2 ),
+            rs = 1 / s;
+
+        return dest.setValue( c.x * rs, c.y * rs, c.z * rs, s * 0.5 );
+      },
+
+      shortestArcQuatNormalize2: function( v0, v1, dest ) {
+        v0.normalize();
+        v1.normalize();
+        return this.shortestArcQuat( v0, v1, dest );
       }
     }
   });
+
+  // Setup temporary variables
+  tmpQ1 = Bump.Quaternion.create();
+  tmpQ2 = Bump.Quaternion.create();
+  tmpV1 = Bump.Vector3.create();
+  tmpV2 = Bump.Vector3.create();
+  tmpV3 = Bump.Vector3.create();
 })( this, this.Bump );
