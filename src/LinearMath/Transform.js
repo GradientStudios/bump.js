19d3ec08463e95b2fb3f610f41b8ff84ab830cb8
Eric Li <li.zhe.hua@gmail.com>
2011-12-21 16:18:00 -0500

    Branch: Transform port (#5)

---
+++ src/LinearMath/Transform.js
@@ -0,0 +1,10 @@
+// **Bump.Transform** is the port of the `btTransform` class in
+// [Bullet](http://bulletphysics.org).
+
+(function( window, Bump ) {
+  Bump.Transform = Bump.type({
+    init: function Transform() {
+
+    }
+  });
+})( this, this.Bump );
