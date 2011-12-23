4d1005195c30bd03ce58c63cee9874da17d971e6
Eric Li <li.zhe.hua@gmail.com>
2011-12-23 12:38:46 -0500

    Overload Transform.create to take in a rotation matrix

--- src/LinearMath/Transform.js
+++ src/LinearMath/Transform.js
@@ -7,29 +7,34 @@
     typeMembers: {
       // ## Constructors
 
-      // Creates a new transform from a [Quaternion](Quaternion.html) `q` and an
+      // Overloaded constructor that creates a new transform from either a
+      // [Quaternion](Quaternion.html) or a [Matrix3x3](Matrix3x3.html), and an
       // optional [Vector3](vector3.html) `c` which represents the translation.
-      create: function( q, c ) {
+      create: function( rot, c ) {
         var transform = Object.create( Bump.Transform.prototype );
-        if ( !q ) {
-          return transform._initEmpty();
+        if ( !rot ) {
+          return transform.init();
         }
 
-        return transform.init( q, c );
+        // The given rotation `rot` is a quaternion.
+        if ( rot.w !== undefined ) {
+          return transform._initWithQuaternion( rot, c );
+        }
+
+        // The given rotation `rot` is a 3x3 rotation matrix.
+        return transform._initWithBasis( rot, c );
       },
 
       // Creates a clone of an `other` transform.
       clone: function( other ) {
         var transform = Object.create( Bump.Transform.prototype );
-        transform._initEmpty();
-        other.basis.clone( transform.basis );
-        other.origin.clone( transform.origin );
+        transform._initWithBasis( other.basis, other.origin );
         return transform;
       },
 
       getIdentity: function() {
         var transform = Object.create( Bump.Transform.prototype );
-        transform._initEmpty();
+        transform.init();
         return transform.setIdentity();
       }
     },
@@ -38,17 +43,23 @@
     // Used by the constructors. Not recommended for normal use.
 
     // Initialize `this` transform with a Quaternion and optional Vector3.
-    init: function Transform( q, c ) {
-      this.basis = Bump.Matrix3x3.createFromQuaternion( q );
-      this.origin = ( c ? c.clone() : Bump.Vector3.create() );
-      return this;
+    init: function Transform() {
+        this.basis = Bump.Matrix3x3.create();
+        this.origin = Bump.Vector3.create();
+        return this;
     },
 
     members: {
+      _initWithBasis: function Transform( b, c ) {
+        this.basis = Bump.Matrix3x3.clone( b );
+        this.origin = ( c ? Bump.Vector3.clone( c ) : Bump.Vector3.create() );
+        return this;
+      },
+
       // Initialize a transform with all zeros.
-      _initEmpty: function Transform() {
-        this.basis = Bump.Matrix3x3.create();
-        this.origin = Bump.Vector3.create();
+      _initWithQuaternion: function Transform( q, c ) {
+        this.basis = Bump.Matrix3x3.createFromQuaternion( q );
+        this.origin = ( c ? c.clone() : Bump.Vector3.create() );
         return this;
       },
 
