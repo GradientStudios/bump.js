476b757505c67a3c8a2b63c925b7fc9dbaebf412
Eric Li <li.zhe.hua@gmail.com>
2012-01-04 12:23:58 -0500

    Branch: CollisionShape port #13

---
+++ src/BulletCollision/CollisionShapes/CollisionShape.js
@@ -0,0 +1,11 @@
+(function( window, Bump ) {
+  Bump.CollisionShape = Bump.type({
+    init: function() {
+
+    },
+
+    members: {
+
+    }
+  });
+})( this, this.Bump );
---
+++ tests/unit/BulletCollision/CollisionShapes/test.CollisionShape.js
@@ -0,0 +1,5 @@
+module( 'Bump.CollisionShape' );
+
+test( 'basic', function() {
+  ok( Bump.CollisionShape, 'CollisionShape exists' );
+});
