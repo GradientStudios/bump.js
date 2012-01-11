b416700307ecace31d5a822d38db2abf4f855561
Eric Li <li.zhe.hua@gmail.com>
2012-01-11 14:31:24 -0500

    Implement CollisionObject interface

--- tests/unit/BulletCollision/CollisionDispatch/test.CollisionObject.js
+++ tests/unit/BulletCollision/CollisionDispatch/test.CollisionObject.js
@@ -1,12 +1,29 @@
 module( 'CollisionObject' );
 
 test( 'creation', function() {
-  ok( Bump.CollisionObject );
+  ok( Bump.CollisionObject, 'CollisionObject exists' );
 
   if ( Bump.CollisionObject ) {
     var co = Bump.CollisionObject.create();
-    ok( co );
+    ok( co, 'creates an object' );
   } else {
     ok( true, 'test skipped' );
   }
+});
+
+test( 'enums', function() {
+  equal( Bump.CollisionObject.CollisionFlags.CF_STATIC_OBJECT,                    1 << 0, 'CF_STATIC_OBJECT'                    );
+  equal( Bump.CollisionObject.CollisionFlags.CF_KINEMATIC_OBJECT,                 1 << 1, 'CF_KINEMATIC_OBJECT'                 );
+  equal( Bump.CollisionObject.CollisionFlags.CF_NO_CONTACT_RESPONSE,              1 << 2, 'CF_NO_CONTACT_RESPONSE'              );
+  equal( Bump.CollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK,         1 << 3, 'CF_CUSTOM_MATERIAL_CALLBACK'         );
+  equal( Bump.CollisionObject.CollisionFlags.CF_CHARACTER_OBJECT,                 1 << 4, 'CF_CHARACTER_OBJECT'                 );
+  equal( Bump.CollisionObject.CollisionFlags.CF_DISABLE_VISUALIZE_OBJECT,         1 << 5, 'CF_DISABLE_VISUALIZE_OBJECT'         );
+  equal( Bump.CollisionObject.CollisionFlags.CF_DISABLE_SPU_COLLISION_PROCESSING, 1 << 6, 'CF_DISABLE_SPU_COLLISION_PROCESSING' );
+
+  equal( Bump.CollisionObject.CollisionObjectTypes.CO_COLLISION_OBJECT, 1 << 0, 'CO_COLLISION_OBJECT' );
+  equal( Bump.CollisionObject.CollisionObjectTypes.CO_RIGID_BODY,       1 << 1, 'CO_RIGID_BODY'       );
+  equal( Bump.CollisionObject.CollisionObjectTypes.CO_GHOST_OBJECT,     1 << 2, 'CO_GHOST_OBJECT'     );
+  equal( Bump.CollisionObject.CollisionObjectTypes.CO_SOFT_BODY,        1 << 3, 'CO_SOFT_BODY'        );
+  equal( Bump.CollisionObject.CollisionObjectTypes.CO_HF_FLUID,         1 << 4, 'CO_HF_FLUID'         );
+  equal( Bump.CollisionObject.CollisionObjectTypes.CO_USER_TYPE,        1 << 5, 'CO_USER_TYPE'        );
 });
