1c56edb91ce66bdc31170214a86fbce74ce8e5a9
Eric Li <li.zhe.hua@gmail.com>
2012-01-12 13:23:10 -0500

    Implement TypedConstraint

--- src/LinearMath/Math.js
+++ src/LinearMath/Math.js
@@ -1,52 +1,77 @@
 // Math stuff that bullet defines.
 (function( window, Bump ) {
+  Bump.SIMD_EPSILON = Math.pow( 2, -52 );
+  Bump.SIMDSQRT12 = Math.sqrt( 1 / 2 );
+  Bump.SIMD_2_PI = 6.283185307179586232;
+  Bump.SIMD_PI = Math.PI;
 
   // Arccosine calculation with bounds checking.
   // Port of `btAcos` from `btScalar.h`.
-  if ( !Bump.Acos ) {
-    Bump.Acos = function btAcos( x ) {
-      if ( x < -1 ) { x = -1; }
-      if ( x >  1 ) { x =  1; }
-      return Math.acos( x );
-    };
-  }
+  Bump.Acos = function btAcos( x ) {
+    if ( x < -1 ) { x = -1; }
+    if ( x >  1 ) { x =  1; }
+    return Math.acos( x );
+  };
 
   // Arcsine calculation with bounds checking.
   // Port of `btAsin` from `btScalar.h`.
-  if ( !Bump.Asin ) {
-    Bump.Asin = function btAsin( x ) {
-      if ( x < -1 ) { x = -1; }
-      if ( x >  1 ) { x =  1; }
-      return Math.asin( x );
-    };
-  }
+  Bump.Asin = function btAsin( x ) {
+    if ( x < -1 ) { x = -1; }
+    if ( x >  1 ) { x =  1; }
+    return Math.asin( x );
+  };
 
-  if ( !Bump.Fsels ) {
-    Bump.Fsels = function btFsels( a, b, c ) {
-      return a >= 0 ? b : c;
-    };
-  }
+  Bump.Fsels = function btFsels( a, b, c ) {
+    return a >= 0 ? b : c;
+  };
 
-  if ( !Bump.Assert ) {
-    Bump.Assert = function btAssert( expression ) {
-      if ( !expression ) {
-        undefined.error();
+  Bump.Fmod = function btFmod( x, y ) {
+    var i = ~~( x / y );
+    return x - i * y;
+  };
+
+  Bump.Assert = function btAssert( expression ) {
+    if ( !expression ) {
+      undefined.error();
+    }
+  };
+
+  Bump.RecipSqrt = function btRecipSqrt( x ) {
+    return 1 / Math.sqrt( x );
+  };
+
+  Bump.NormalizeAngle = function btNormalizeAngle( angleInRadians ) {
+    angleInRadians = Bump.Fmod( angleInRadians, Bump.SIMD_2_PI );
+    if ( angleInRadians < -Math.PI ) {
+      return angleInRadians + Math.PI;
+    } else if( angleInRadians > Math.PI ) {
+      return angleInRadians - Math.PI;
+    } else {
+      return angleInRadians;
+    }
+  };
+
+  Bump.TypedObject = Bump.type({
+    init: function TypedObject( objectType ) {
+      this.objectType = objectType;
+    },
+
+    members: {
+      clone: function( dest ) {
+        dest = dest || Bump.TypedObject.create();
+        dest.objectType = this.objectType;
+        return dest;
+      },
+
+      assign: function( other ) {
+        this.objectType = other.objectType;
+        return this;
+      },
+
+      getObjectType: function() {
+        return this.objectType;
       }
-    };
-  }
-
-  if ( !Bump.RecipSqrt ) {
-    Bump.RecipSqrt = function btRecipSqrt( x ) {
-      return 1 / Math.sqrt( x );
-    };
-  }
-
-  if ( Bump.SIMD_EPSILON === undefined ) {
-    Bump.SIMD_EPSILON = Math.pow( 2, -52 );
-  }
-
-  if ( Bump.SIMDSQRT12 === undefined ) {
-    Bump.SIMDSQRT12 = Math.sqrt( 1 / 2 );
-  }
+    }
+  });
 
 })( this, this.Bump );
