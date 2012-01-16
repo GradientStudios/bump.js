a7ead9146d184ad538a5ae79f9d5745e6ba9c0e1
Andrew Dolce <andrew.dolce@gmail.com>
2012-01-16 11:59:28 -0500

    Ported DbvtProxy

--- src/BulletCollision/BroadphaseCollision/DbvtBroadphase.js
+++ src/BulletCollision/BroadphaseCollision/DbvtBroadphase.js
@@ -1,5 +1,17 @@
 (function( window, Bump ) {
 
+  Bump.DbvtProxy = Bump.type( {
+    parent: Bump.BroadphaseProxy,
+
+    init: function DbvtProxy( aabbMin, aabbMax, userPtr, collisionFilterGroup, collisionFilterMask ) {
+      this._super( aabbMin, aabbMax, userPtr, collisionFilterGroup, collisionFilterMask );
+
+      this.leaf = 0;
+      this.links = [ 0, 0 ];
+      this.stage = 0;
+    }
+  } );
+
   Bump.DbvtBroadphase = Bump.type( {
     parent: Bump.BroadphaseInterface
   } );
