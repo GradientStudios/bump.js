36f3d9d9e82bf83b8b6a5f0fa36aa8b144c979cf
Andrew Dolce <andrew.dolce@gmail.com>
2012-01-19 17:08:41 -0500

    Ported some HashedOverlappingPairCache member functions

--- src/BulletCollision/BroadphaseCollision/OverlappingPairCache.js
+++ src/BulletCollision/BroadphaseCollision/OverlappingPairCache.js
@@ -14,10 +14,10 @@
     }
   } );
 
-  // Find these:
-  // extern int gRemovePairs;
-  // extern int gAddedPairs;
-  // extern int gFindPairs;
+  // These globals shouldn't need to be accessible outside of OverlappingPairCache.js.
+  var gRemovePairs = 0,
+      gAddedPairs = 0,
+      gFindPairs = 0;
 
   // *** OverlappingPairCache *** is the port of the original bullet class `btOverlappingPairCallback`.
   /// The btOverlappingPairCache provides an interface for overlapping pair management
@@ -56,7 +56,60 @@
     }
 
     members: {
-      removeOverlappingPairsContainingProxy( proxy, dispatched ) {}
+      removeOverlappingPairsContainingProxy: function( proxy, dispatched ) {}, // TODO
+      removeOverlappingPair: function( proxy0, proxy1, dispatcher ) {}, // TODO
+
+      needsBroadphaseCollision: function( proxy0, proxy1 ) {
+        if( this.m_overlapFilterCallback ) {
+          return this.m_overlapFilterCallback.needBroadphaseCollision( proxy0, proxy1 );
+        }
+        var collides = ( proxy0.m_collisionFilterGroup & proxy1.m_collisionFilterMask ) !== 0;
+        collides = collides && ( proxy1.m_collisionFilterGroup & proxy0.m_collisionFilterMask );
+        return collides;
+      },
+
+      // Add a pair and return the new pair. If the pair already exists,
+      // no new pair is created and the old one is returned.
+      addOverlappingPair: function( proxy0, proxy1 ) {
+        gAddedPairs++;
+        if( !this.needsBroadphaseCollision( proxy0, proxy1 ) ) {
+          return 0;
+        }
+        return this.internalAddPair( proxy0, proxy1 );
+      },
+
+      cleanProxyFromPairs: function( proxy,btDispatcher* dispatcher) {}, // TODO
+      processAllOverlappingPairs: function( callback, dispatcher) {}, // TODO
+
+      getOverlappingPairArrayPtr: function() {
+        return this.m_overlappingPairArray;
+      },
+
+      getOverlappingPairArray: function() {
+        return this.m_overlappingPairArray;
+      },
+
+      cleanOverlappingPair: function( pair, dispatcher ) {}, // TODO
+
+      findPair: function( proxy0, proxy1 ) {}, // TODO
+
+      GetCount: function() {
+        return this.m_overlappingPairArray.length();
+      },
+      /* btBroadphasePair* GetPairs() { return m_pairs; } */
+
+      getOverlapFilterCallback: function() {
+        return this.m_overlapFilterCallback;
+      },
+
+      setOverlapFilterCallback: function( callback ) {
+        this.m_overlapFilterCallback = callback;
+      },
+
+      getNumOverlappingPairs: function() {
+        return this.m_overlappingPairArray.length();
+      }
+
     }
   } );
 
