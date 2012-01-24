a39cc2b9248d8abfbc3b364f48cf0e454b16e6c1
Eric Li <li.zhe.hua@gmail.com>
2012-01-24 10:22:55 -0500

    Port UnionFind. Closes #50.

--- src/LinearMath/AlignedObjectArray.js
+++ src/LinearMath/AlignedObjectArray.js
@@ -49,4 +49,31 @@
       quickSortInternal( arr, compareFunc, 0, arr.length() - 1 );
     }
   };
+
+  // *** Bump.resize *** is the `resize` member function of `btAlignedObjectArray`.
+  // Given an Javascript Array `arr`, will change the number of elements in the
+  // array. If the new size is larger, the new elements will be constructed
+  // using `clone`s of the `fillData`. If the new size is smaller, the
+  // destructor will be called.
+  Bump.resize = function( arr, newsize, fillData ) {
+    var i, curSize = arr.length;
+
+    if ( newsize < curSize ) {
+      for ( i = newsize; i < curSize; ++i ) {
+        if ( arr[i].destruct ) {
+          arr[i].destruct();
+        }
+      }
+      arr.length = newsize;
+    } else {
+      if ( newsize > arr.length ) {
+        arr.length = newsize;
+      }
+
+      for ( i = curSize; i < newsize; ++i ) {
+        arr[ i ] = fillData.clone();
+      }
+    }
+  };
+
 } )( this, this.Bump );
---
+++ tests/unit/BulletCollision/CollisionDispatch/test.UnionFind.js
@@ -0,0 +1,137 @@
+module( 'UnionFind.create' );
+
+test( 'basic', function() {
+  var uf = Bump.UnionFind.create();
+
+  ok( uf instanceof Bump.UnionFind.prototype.constructor, 'correct type' );
+});
+
+module( 'UnionFind.clone' );
+
+test( 'basic', function() {
+  var i, uf = Bump.UnionFind.create();
+  uf.allocate( 4 );
+  for ( i = 0; i < 4; ++i ) {
+    uf.getElement( i ).id = i;
+  }
+
+  var clone = uf.clone();
+
+  deepEqual( uf, clone );
+  for ( i = 0; i < 4; ++i ) {
+    strictEqual( clone.getElement( i ).id, i );
+    notStrictEqual( uf.getElement( i ), clone.getElement( i ) );
+  }
+});
+
+module( 'UnionFind.assign' );
+
+test( 'test skipped', function() {});
+
+module( 'UnionFind.sortIslands' );
+
+test( 'test skipped', function() {});
+
+module( 'UnionFind.reset' );
+
+test( 'test skipped', function() {});
+
+module( 'UnionFind.getNumElements' );
+
+test( 'basic', function() {
+  var uf = Bump.UnionFind.create();
+  uf.allocate( 4 );
+
+  testFunc( Bump.UnionFind, 'getNumElements', {
+    objects: uf,
+    expected: [ 4 ]
+  });
+});
+
+module( 'UnionFind.isRoot' );
+
+test( 'basic', function() {
+  var uf = Bump.UnionFind.create();
+  uf.allocate( 5 );
+  for ( var i = 0; i < 5; ++i ) {
+    uf.getElement( i ).id = 4 - i;
+  }
+
+  testFunc( Bump.UnionFind, 'isRoot', {
+    objects: uf,
+    args: [ [ 0 ], [ 1 ], [ 2 ], [ 3 ], [ 4 ] ],
+    expected: [ false, false, true, false, false ]
+  });
+});
+
+module( 'UnionFind.getElement' );
+
+test( 'basic', function() {
+  var uf = Bump.UnionFind.create();
+  uf.allocate( 5 );
+
+  testFunc( Bump.UnionFind, 'getElement', {
+    objects: uf,
+    args: [ [ 0 ], [ 1 ], [ 2 ], [ 3 ], [ 4 ] ],
+    expected: [
+      uf.elements[0],
+      uf.elements[1],
+      uf.elements[2],
+      uf.elements[3],
+      uf.elements[4]
+    ]
+  });
+});
+
+module( 'UnionFind.allocate' );
+
+test( 'basic', function() {
+  var i, count, uf = Bump.UnionFind.create();
+  strictEqual( uf.getNumElements(), 0, 'starts with zero elements' );
+
+  uf.allocate( 4 );
+  strictEqual( uf.getNumElements(), 4, 'allocates 4 elements' );
+
+  var refs = [];
+  count = 0;
+  for ( i = 0; i < 4; ++i ) {
+    refs.push( uf.getElement( i ) );
+    if ( uf.getElement( i ) instanceof Bump.Element.prototype.constructor ) {
+      ++count;
+    }
+  }
+  strictEqual( count, 4, 'initializes elements' );
+
+  uf.allocate( 8 );
+  for ( i = 0; i < refs.length; ++i ) {
+    strictEqual( uf.getElement( i ), refs[ i ], 'keeps references' );
+  }
+
+  count = 0;
+  for ( i = refs.length; i < uf.getNumElements(); ++i ) {
+    if ( uf.getElement( i ) instanceof Bump.Element.prototype.constructor ) {
+      ++count;
+    }
+  }
+  strictEqual( count, 4, 'initializes new elements at end' );
+
+  uf.allocate( 4 );
+  strictEqual( uf.getNumElements(), 4, 'back down to 4 elements' );
+
+  for ( i = 0; i < refs.length; ++i ) {
+    strictEqual( uf.getElement( i ), refs[ i ], 'keeps references' );
+  }
+});
+
+module( 'UnionFind.Free' );
+
+test( 'test skipped', function() {});
+
+module( 'UnionFind.find' );
+
+test( 'one arg - test skipped', function() {});
+test( 'two args - test skipped', function() {});
+
+module( 'UnionFind.unite' );
+
+test( 'test skipped', function() {});
