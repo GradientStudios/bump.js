b173f716a6723be4bdce94da6147086a4c9291e7
Andrew Dolce <andrew.dolce@gmail.com>
2012-04-26 18:00:15 -0400

    Fixed incorrect file order in build.json and test/index.html. Added unit tests for DefaultVehicleRaycaster creation.

--- tests/unit/BulletDynamics/Vehicle/test.RaycastVehicle.js
+++ tests/unit/BulletDynamics/Vehicle/test.RaycastVehicle.js
@@ -1,3 +1,26 @@
 module( 'DefaultVehicleRaycaster.create' );
 
-test( 'test skipped', function() {});
+test( 'basic', function() {
+  var rv = Bump.DefaultVehicleRaycaster.create();
+
+  ok( rv instanceof Bump.VehicleRaycaster.prototype.constructor );
+});
+
+test( 'correctTypes', function() {
+  var cc = Bump.DefaultCollisionConfiguration.create();
+  var d = Bump.CollisionDispatcher.create( cc );
+  var opc = Bump.DbvtBroadphase.create();
+  var dw = Bump.DynamicsWorld.create( d, opc, cc );
+
+  var rv = Bump.DefaultVehicleRaycaster.create( dw );
+
+  var checks = [
+    [ 'dynamicsWorld', Bump.DynamicsWorld ],
+    [ 'distFraction', 'number' ],
+    [ 'hitPointInWorld', Bump.Vector3 ],
+    [ 'hitNormalInWorld', Bump.Vector3 ],
+    [ 'distFraction', 'number' ]
+  ];
+
+  checkTypes( rv, checks );
+});
