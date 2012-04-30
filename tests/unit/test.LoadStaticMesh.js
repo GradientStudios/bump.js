b95702c7c7dd3e11a5b7d66b2163b1409d37b2a8
Eric Li <li.zhe.hua@gmail.com>
2012-04-30 12:41:24 -0400

    Update test with ScaledBvhTriangleMeshShape

--- tests/unit/test.LoadStaticMesh.js
+++ tests/unit/test.LoadStaticMesh.js
@@ -1,5 +1,23 @@
 module( 'LoadStaticMesh' );
 
 test( 'basic load OBJ', function() {
+  var mesh = Bump.TriangleMesh.create();
+  ok( mesh instanceof Bump.TriangleMesh.prototype.constructor );
+
+  mesh.addTriangle(
+    Bump.Vector3.create( 0, 0, 0 ),
+    Bump.Vector3.create( 1, 1, 0 ),
+    Bump.Vector3.create( 0, 1, 1 ),
+    true
+  );
+
+  var shape = Bump.BvhTriangleMeshShape.create( mesh, true );
+  ok( shape instanceof Bump.BvhTriangleMeshShape.prototype.constructor );
+
+  var scaledShape = Bump.ScaledBvhTriangleMeshShape.create(
+    shape,
+    Bump.Vector3.create( 1, 1, 1 )
+  );
+  ok( scaledShape instanceof Bump.ScaledBvhTriangleMeshShape.prototype.constructor );
 
 });
